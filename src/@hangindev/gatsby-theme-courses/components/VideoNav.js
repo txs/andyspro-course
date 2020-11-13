a0551a309bb98f09f4a8aaad6f96abc591af0268
Andy tseng <txshon@Andys-MacBook-Pro.local>
2020-11-14 01:22:58 +0800

    Next and back 中文化

---
+++ src/@hangindev/gatsby-theme-courses/components/VideoNav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+import { usePageValue } from '../context/PageContext';
+
+const Nav = styled.nav`
+  display: flex;
+  padding: 0.5rem;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 0.8rem;
+`;
+
+function VideoNav({ className }) {
+    const { previousLesson, nextLesson } = usePageValue();
+    return (
+        <Nav className={className}>
+            {previousLesson ? (
+                <Link to={previousLesson.slug}>
+                    <span>⬅️ 上一堂</span>
+                </Link>
+            ) : (
+                    <div />
+                )}
+            {nextLesson && (
+                <Link to={nextLesson.slug}>
+                    <span>下一堂 ➡️</span>
+                </Link>
+            )}
+        </Nav>
+    );
+}
+
+export default VideoNav;
