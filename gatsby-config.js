a79f22e178c5bb06ff2f9053d886d8b478a2d75d
Andy tseng <txshon@Andys-MacBook-Pro.local>
2020-11-13 20:05:10 +0800

    change google-analytics to gtags

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,21 @@ module.exports = {
         {
             resolve: `@hangindev/gatsby-theme-courses`,
         },
+        // {
+        //     resolve: `gatsby-plugin-google-analytics`,
+        //     options: {
+        //         trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+        //     },
+        // },
         {
-            resolve: `gatsby-plugin-google-analytics`,
+            resolve: `gatsby-plugin-google-gtag`,
             options: {
-                trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+                // You can add multiple tracking ids and a pageview event will be fired for all of them.
+                trackingIds: [
+                    process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none", // Google Analytics / GA
+                    // "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+                    // "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+                ],
             },
         },
     ],
