90ceef35f9892ccc71a292b2987fefbef41af780
Andy tseng <txshon@Andys-MacBook-Pro.local>
2020-11-13 16:50:05 +0800

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,10 +3,17 @@ module.exports = {
         {
             resolve: `@hangindev/gatsby-theme-courses`,
         },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || "none",
+            },
+        },
     ],
     siteMetadata: {
         title: `安迪的 PR0 課程`,
-        author: `txs`,
+        author: `@txs`,
+        siteUrl: `https://course.andys.pro`,
         description: `影音線上程式設計課程平台`,
         social: [
             {
